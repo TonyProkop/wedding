b047fe64d59746f76504fc46f26950d82a405a33
TonyProkop <prokop.tony@yahoo.com>
2019-07-19 07:39:02 -0500

    Updated header menu.

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,31 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import styles from "./header.module.scss"
 import scrollIntoView from "../utils/scrollIntoView"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
     <header id="header" className={styles.headerContainer}>
-      <FontAwesomeIcon icon="bars"></FontAwesomeIcon>
-      <ul className={styles.headerMenu}>
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li onClick={() => scrollIntoView("weddingEvents")}>
-          Wedding Events
-        </li>
-        <li onClick={() => scrollIntoView("registry")}>
-          Registry
-        </li>
-        <li onClick={() => scrollIntoView("rsvp")}>
-          RSVP
-        </li>
-      </ul>
+      <FontAwesomeIcon icon="bars" onClick={() => setMenuActive(!menuActive)}></FontAwesomeIcon>
+      {
+        menuActive 
+        ? <ul className={styles.headerMenu}>
+          <li onClick={() => scrollIntoView("weddingEvents")}>
+            Wedding Events
+          </li>
+          <li onClick={() => scrollIntoView("registry")}>
+            Registry
+          </li>
+          <li onClick={() => scrollIntoView("rsvp")}>
+            RSVP
+          </li>
+        </ul>
+        : false
+      }
+
     </header>
   )
 }
