639cec7045b9d6a03b032f0f5fb9f18cc74881d6
TonyProkop <prokop.tony@yahoo.com>
2019-09-11 07:19:46 -0500

    More work on header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,91 @@
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useState, Fragment } from "react"
 import styles from "./header.module.scss"
 import scrollIntoView from "../utils/scrollIntoView"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
+// const EventListeners = () => {
+//   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+//   const [heightFromTop, setHeightFromTop] = useState(
+//     document.documentElement.scrollTop
+//   )
+
+//   window.addEventListener("resize", setHeightFromTop(window.innerWidth))
+//   window.addEventListener(
+//     "scroll",
+//     setHeightFromTop(document.documentElement.scrollTop)
+//   )
+
+//   return <Header screenWidth={screenWidth} heightFromTop={heightFromTop} />
+// }
+
 const Header = () => {
+  // let sticky = false
+  // window.addEventListener("scroll", function(e) {
+  //   sticky = document.documentElement.scrollTop > window.innerHeight
+  // })
+
+  if (window.innerWidth > 700) {
+    return (
+      <header
+        id="header"
+        className={`${styles.headerContainer} ${styles.desktop}`}
+      >
+        <DesktopMenu />
+      </header>
+    )
+  } else {
+    return (
+      <header
+        id="header"
+        className={`${styles.headerContainer} ${styles.mobile}`}
+      >
+        <MobileMenu />
+      </header>
+    )
+  }
+}
+
+const DesktopMenu = () => (
+  <ul className={styles.headerMenu}>
+    <li onClick={() => scrollIntoView("story")}>Our Story</li>
+    <li onClick={() => scrollIntoView("weddingEvents")}>Wedding Details</li>
+    <li onClick={() => scrollIntoView("photos")}>Photos</li>
+    <li onClick={() => scrollIntoView("registry")}>Registry</li>
+    <li onClick={() => scrollIntoView("rsvp")} className={styles.rsvp}>
+      RSVP
+    </li>
+  </ul>
+)
+
+const MobileMenu = () => {
   const [menuActive, setMenuActive] = useState(false)
 
-  let sticky = false
-  window.addEventListener("scroll", function(e) {
-    sticky = document.documentElement.scrollTop > window.innerHeight
-  })
-
-  return (
-    <header
-      id="header"
-      className={`${styles.headerContainer} ${sticky ? "sticky" : false}`}
-    >
-      <ul className={styles.headerMenu}>
-        <li onClick={() => scrollIntoView("story")}>Our Story</li>
-        <li onClick={() => scrollIntoView("weddingEvents")}>Wedding Details</li>
-        <li onClick={() => scrollIntoView("photos")}>Photos</li>
-        <li onClick={() => scrollIntoView("registry")}>Registry</li>
-        <li onClick={() => scrollIntoView("rsvp")} className={styles.rsvp}>
-          RSVP
-        </li>
-      </ul>
-    </header>
-  )
+  if (menuActive) {
+    return (
+      <Fragment>
+        <FontAwesomeIcon
+          icon="times"
+          onClick={() => setMenuActive(!menuActive)}
+        />
+        <ul className={styles.headerMenu}>
+          <li onClick={() => scrollIntoView("story")}>Our Story</li>
+          <li onClick={() => scrollIntoView("weddingEvents")}>
+            Wedding Details
+          </li>
+          <li onClick={() => scrollIntoView("photos")}>Photos</li>
+          <li onClick={() => scrollIntoView("registry")}>Registry</li>
+          <li onClick={() => scrollIntoView("rsvp")} className={styles.rsvp}>
+            RSVP
+          </li>
+        </ul>
+      </Fragment>
+    )
+  } else {
+    return (
+      <FontAwesomeIcon icon="bars" onClick={() => setMenuActive(!menuActive)} />
+    )
+  }
 }
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,19 +3,35 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import styles from "./layout.module.scss"
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faGlassCheers, faChurch, faGift, faChevronDown, faChevronRight, faBars } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGlassCheers,
+  faChurch,
+  faGift,
+  faChevronDown,
+  faChevronRight,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons"
 
-library.add(faGlassCheers, faChurch, faGift, faChevronDown, faChevronRight, faBars)
+library.add(
+  faGlassCheers,
+  faChurch,
+  faGift,
+  faChevronDown,
+  faChevronRight,
+  faBars,
+  faTimes
+)
 
 const Layout = ({ children }) => (
   <Fragment>
     <Header />
     <div className={styles.pageContainer}>
       <main>{children}</main>
-      <footer></footer>
+      <footer />
     </div>
   </Fragment>
-);
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
