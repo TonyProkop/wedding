4ff856abb91d61b1acc5513922d1a4d522f90329
Tony Prokop <13754823+TonyProkop@users.noreply.github.com>
2019-09-11 06:37:59 -0500

    Started header refactor.

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,30 @@ import PropTypes from "prop-types"
 import React, { useState } from "react"
 import styles from "./header.module.scss"
 import scrollIntoView from "../utils/scrollIntoView"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header = () => {
-  const [menuActive, setMenuActive] = useState(false);
+  const [menuActive, setMenuActive] = useState(false)
+
+  let sticky = false
+  window.addEventListener("scroll", function(e) {
+    sticky = document.documentElement.scrollTop > window.innerHeight
+  })
 
   return (
-    <header id="header" className={styles.headerContainer}>
-      <FontAwesomeIcon icon="bars" onClick={() => setMenuActive(!menuActive)}></FontAwesomeIcon>
-      {
-        menuActive 
-        ? <ul className={styles.headerMenu}>
-          <li onClick={() => scrollIntoView("weddingEvents")}>
-            Wedding Events
-          </li>
-          <li onClick={() => scrollIntoView("registry")}>
-            Registry
-          </li>
-          <li onClick={() => scrollIntoView("rsvp")}>
-            RSVP
-          </li>
-        </ul>
-        : false
-      }
-
+    <header
+      id="header"
+      className={`${styles.headerContainer} ${sticky ? "sticky" : false}`}
+    >
+      <ul className={styles.headerMenu}>
+        <li onClick={() => scrollIntoView("story")}>Our Story</li>
+        <li onClick={() => scrollIntoView("weddingEvents")}>Wedding Details</li>
+        <li onClick={() => scrollIntoView("photos")}>Photos</li>
+        <li onClick={() => scrollIntoView("registry")}>Registry</li>
+        <li onClick={() => scrollIntoView("rsvp")} className={styles.rsvp}>
+          RSVP
+        </li>
+      </ul>
     </header>
   )
 }
