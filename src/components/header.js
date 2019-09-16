68ed6f70d864101659899969612ee66a4f1e1471
TonyProkop <prokop.tony@yahoo.com>
2019-09-16 06:46:26 -0500

    Fixed header bug in prod

--- src/components/header.js
+++ src/components/header.js
@@ -1,85 +1,96 @@
 import PropTypes from "prop-types"
-import React, { useState } from "react"
-import { useMediaQuery } from "react-responsive"
+import React, { useState, useEffect, Fragment } from "react"
 import styles from "./header.module.scss"
 import scrollIntoView from "../utils/scrollIntoView"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header = () => {
-  const isDesktop = useMediaQuery({ query: "(min-width: 700px)" })
-  const [menuActive, setMenuActive] = useState(false)
+  try {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 700)
+    const [menuActive, setMenuActive] = useState(false)
 
-  const scrollIntoViewAndCloseMenu = id => {
-    scrollIntoView(id)
-    setMenuActive(false)
-  }
+    const scrollIntoViewAndCloseMenu = id => {
+      scrollIntoView(id)
+      setMenuActive(false)
+    }
 
-  if (isDesktop) {
+    useEffect(() => {
+      window.addEventListener("resize", function() {
+        if (window.innerWidth < 700) {
+          setIsMobile(true)
+        } else {
+          setIsMobile(false)
+        }
+      })
+    }, [])
+
+    let menu = (
+      <ul className={styles.headerMenu}>
+        <li onClick={() => scrollIntoView("story")}>Our Story</li>
+        <li onClick={() => scrollIntoView("photos")}>Photos</li>
+        <li onClick={() => scrollIntoView("weddingEvents")}>Wedding Details</li>
+        <li onClick={() => scrollIntoView("registry")}>Registry</li>
+        <li onClick={() => scrollIntoView("rsvp")} className={styles.rsvp}>
+          RSVP
+        </li>
+      </ul>
+    )
+
+    if (isMobile) {
+      if (menuActive) {
+        menu = (
+          <Fragment>
+            <div>
+              <FontAwesomeIcon
+                icon="times"
+                className={styles.closeMenu}
+                onClick={() => setMenuActive(!menuActive)}
+              />
+            </div>
+
+            <ul className={styles.headerMenu}>
+              <li onClick={() => scrollIntoViewAndCloseMenu("story")}>
+                Our Story
+              </li>
+              <li onClick={() => scrollIntoViewAndCloseMenu("weddingEvents")}>
+                Wedding Details
+              </li>
+              <li onClick={() => scrollIntoViewAndCloseMenu("photos")}>
+                Photos
+              </li>
+              <li onClick={() => scrollIntoViewAndCloseMenu("registry")}>
+                Registry
+              </li>
+              <li
+                onClick={() => scrollIntoViewAndCloseMenu("rsvp")}
+                className={styles.rsvp}
+              >
+                RSVP
+              </li>
+            </ul>
+          </Fragment>
+        )
+      } else {
+        menu = (
+          <FontAwesomeIcon
+            icon="bars"
+            onClick={() => setMenuActive(!menuActive)}
+          />
+        )
+      }
+    }
+
     return (
       <header
-        id="header"
-        className={`${styles.headerContainer} ${styles.desktop}`}
+        className={`${styles.headerContainer} ${
+          isMobile ? styles.mobile : false
+        } ${menuActive ? styles.active : false}`}
       >
-        <ul className={styles.headerMenu}>
-          <li onClick={() => scrollIntoViewAndCloseMenu("story")}>Our Story</li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("photos")}>Photos</li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("weddingEvents")}>
-            Wedding Details
-          </li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("registry")}>
-            Registry
-          </li>
-          <li
-            onClick={() => scrollIntoViewAndCloseMenu("rsvp")}
-            className={styles.rsvp}
-          >
-            RSVP
-          </li>
-        </ul>
+        {menu}
       </header>
     )
-  } else if (menuActive) {
-    return (
-      <header
-        id="header"
-        className={`${styles.headerContainer} ${styles.mobile} ${
-          styles.active
-        }`}
-      >
-        <FontAwesomeIcon
-          icon="times"
-          onClick={() => setMenuActive(!menuActive)}
-        />
-        <ul className={styles.headerMenu}>
-          <li onClick={() => scrollIntoViewAndCloseMenu("story")}>Our Story</li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("weddingEvents")}>
-            Wedding Details
-          </li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("photos")}>Photos</li>
-          <li onClick={() => scrollIntoViewAndCloseMenu("registry")}>
-            Registry
-          </li>
-          <li
-            onClick={() => scrollIntoViewAndCloseMenu("rsvp")}
-            className={styles.rsvp}
-          >
-            RSVP
-          </li>
-        </ul>
-      </header>
-    )
-  } else {
-    return (
-      <header
-        id="header"
-        className={`${styles.headerContainer} ${styles.mobile}`}
-      >
-        <FontAwesomeIcon
-          icon="bars"
-          onClick={() => setMenuActive(!menuActive)}
-        />
-      </header>
-    )
+  } catch (e) {
+    return false
   }
 }
 
