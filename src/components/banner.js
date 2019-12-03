211ac37fbc997692c22c5736f8cbffd3b92ec92e
Tony Prokop <13754823+TonyProkop@users.noreply.github.com>
2019-12-03 13:07:15 -0600

    Carousel improvements.

--- src/components/banner.js
+++ src/components/banner.js
@@ -10,23 +10,23 @@ import img4 from "../images/katie_and_me/Tony+Katie-174.jpg"
 
 const Banner = () => {
   return (
-    // <div className={styles.bannerContainer}>
-    //   <div className={styles.bannerText}>
-    //     <div>The Wedding Celebration of</div>
-    //     <h1 className={styles.bannerTitle}>Tony & Katie</h1>
-    //     <div>May 2, 2020</div>
-    //   </div>
-    //   <FontAwesomeIcon
-    //     icon="chevron-down"
-    //     onClick={() => scrollIntoView("story")}
-    //   />
-    // </div>
-    <Carousel>
-      <img src={img1} />
-      <img src={img2} />
-      <img src={img3} />
-      <img src={img4} />
-    </Carousel>
+    <div className={styles.bannerContainer}>
+      <div className={styles.bannerText}>
+        <div>The Wedding Celebration of</div>
+        <h1 className={styles.bannerTitle}>Tony & Katie</h1>
+        <div>May 2, 2020</div>
+      </div>
+      {/* <FontAwesomeIcon
+        icon="chevron-down"
+        onClick={() => scrollIntoView("story")}
+      /> */}
+      <Carousel rotateDuration={10000}>
+        <img src={img1} />
+        <img src={img2} />
+        <img src={img3} />
+        <img src={img4} />
+      </Carousel>
+    </div>
   )
 }
 
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,21 +1,25 @@
 import React, { Fragment, useState, useEffect } from "react"
+import classnames from "classnames"
 import styles from "./carousel.module.scss"
 
-const Carousel = ({ children }) => {
+const Carousel = ({ children, rotateDuration }) => {
   const [activeChild, setActiveChild] = useState(0)
 
   useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setActiveChild(activeChild =>
-          activeChild == children.length - 1 ? 0 : activeChild + 1
-        ),
-      1000
-    )
-    return () => {
-      window.clearInterval(interval)
+    if (rotateDuration) {
+      const interval = setInterval(
+        () =>
+          setActiveChild(activeChild =>
+            activeChild == children.length - 1 ? 0 : activeChild + 1
+          ),
+        rotateDuration
+      )
+
+      return () => {
+        window.clearInterval(interval)
+      }
     }
-  }, [])
+  }, [rotateDuration, activeChild])
 
   return (
     <div className={styles.carouselContainer}>
@@ -27,9 +31,18 @@ const Carousel = ({ children }) => {
       >
         {children}
       </div>
-      <div>
+      <div className={styles.control}>
         {children.map((x, i) => {
-          return <div onClick={() => setActiveChild(i)}>{i}</div>
+          return (
+            <div
+              className={classnames(
+                styles.knob,
+                i === activeChild ? styles.active : ""
+              )}
+              key={i}
+              onClick={() => setActiveChild(i)}
+            />
+          )
         })}
       </div>
     </div>
