9fe2f78b842da4e4c5d9103ee947238f4d7e16f7
TonyProkop <prokop.tony@yahoo.com>
2019-12-18 06:57:46 -0600

    RSVP form improvements

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
+import classnames from "classnames"
 import styles from "./rsvp.module.scss"
 
 const RSVP = () => {
+  const [cantMakeIt, setCantMakeIt] = useState(false)
+  const [numbGuests, setNumbGuests] = useState("")
+
   return (
     <div className={styles.rsvpSection}>
       <div className={styles.pageWidth}>
@@ -21,26 +25,31 @@ const RSVP = () => {
               type="text"
               name="fullName"
               placeholder="Full Name"
-              required="true"
+              required={true}
             />
-            <br />
-            <label className={styles.numbGuestsSelect}>
-              Number of Guests
-              <select required="required" name="numberOfGuests">
-                <option value="0">None</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-                <option value="4">Four</option>
-                <option value="5">Five</option>
-                <option value="6">Six</option>
-                <option value="7">Seven</option>
-                <option value="8">Eight</option>
-                <option value="9">Nine</option>
-                <option value="10">Ten</option>
-              </select>
-            </label>
-            <br />
+            <select
+              className={classnames(
+                styles.numbGuestsSelect,
+                cantMakeIt ? styles.hide : ""
+              )}
+              required={!cantMakeIt}
+              name="numberOfGuests"
+              value={numbGuests}
+              onChange={e => setNumbGuests(e.target.value)}
+            >
+              <option value="">Number of Guests</option>
+              <option value="0">None</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+              <option value="4">Four</option>
+              <option value="5">Five</option>
+              <option value="6">Six</option>
+              <option value="7">Seven</option>
+              <option value="8">Eight</option>
+              <option value="9">Nine</option>
+              <option value="10">Ten</option>
+            </select>
             <textarea
               className={styles.rsvpInput}
               type="text"
@@ -48,10 +57,23 @@ const RSVP = () => {
               name="message"
             />
             <label>
-              <input className={styles.rsvpInput} type="checkbox" />I can't make
-              it
+              <input
+                className={styles.rsvpInput}
+                type="checkbox"
+                name="cantMakeIt"
+                value={cantMakeIt}
+                onChange={() => {
+                  const newCantMakeIt = !cantMakeIt
+                  setCantMakeIt(newCantMakeIt)
+                  if (newCantMakeIt) {
+                    setNumbGuests(0)
+                  } else {
+                    setNumbGuests("")
+                  }
+                }}
+              />
+              I can't make it
             </label>
-            <br />
             <input name="bot-field" className={styles.honeypot} />
             <input type="submit" className={styles.button} value="SUBMIT" />
           </form>
