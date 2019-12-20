309a21f1713fde270413be15c3dc38ad68dc3a3b
TonyProkop <prokop.tony@yahoo.com>
2019-12-20 07:05:08 -0600

    This polyfill thing didnt' work

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,32 +9,6 @@ import RSVP from "../components/rsvp"
 import Countdown from "../components/countdown"
 
 const IndexPage = () => {
-  useEffect(() => {
-    if ("objectFit" in document.documentElement.style === false) {
-      document.addEventListener("DOMContentLoaded", function() {
-        Array.prototype.forEach.call(
-          document.querySelectorAll("img[data-object-fit]"),
-          function(image) {
-            ;(image.runtimeStyle || image.style).background =
-              'url("' +
-              image.src +
-              '") no-repeat 50%/' +
-              (image.currentStyle
-                ? image.currentStyle["object-fit"]
-                : image.getAttribute("data-object-fit"))
-
-            image.src =
-              "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" +
-              image.width +
-              "' height='" +
-              image.height +
-              "'%3E%3C/svg%3E"
-          }
-        )
-      })
-    }
-  }, [typeof document])
-
   return (
     <Layout>
       <SEO title="Tony & Katie's Wedding" />
