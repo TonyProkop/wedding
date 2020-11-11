fc6c2feb552692cd0f765844c3ed9b5339409b90
Tony Prokop <tony.prokop@getflywheel.com>
2020-11-11 12:12:41 -0600

    Updated covid rules.

--- src/components/covid.js
+++ src/components/covid.js
@@ -19,11 +19,11 @@ const Covid = () => {
 						<h1>COVID-19</h1>
 						<p>What we're doing to stop the spread:</p>
 						<ul className={styles.precautionList}>
-							<li>Masks available</li>
-							<li>Hand sanitizer provided</li>
-							<li>Color coded wrist bands for social distance preference</li>
-							<li>Must pass thermometer test to enter reception</li>
-							<li>Our venue and caterer are also going above and beyond to protect the health of our guests.</li>
+							<li>Masks are required unless seated at a table</li>
+							<li>Tables are 6' apart, max 8 people per table</li>
+							<li>Hand sanitizer available</li>
+							<li>Dancing must be done in 8 person groups around a table</li>
+							<li>Must pass contact-free temperature check before entering reception</li>
 							<li>Feel free to reach out for more details if needed!</li>
 						</ul>
 					</div>
